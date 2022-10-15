21d6b8393f6bf2f20cd714a969022edcc1b894d0
Max <max.heinzer397@gmx.ch>
2022-10-15 13:50:49 +0200

    refactor: use Vec<char> instead of string

--- src/main.rs
+++ src/main.rs
@@ -4,56 +4,55 @@ fn main() {
         println!("Input some brainfuck code as argument");
         return;
     }
-    let bf_code = &args[1];
+    let instructions: Vec<char> = (&args[1]).chars().collect();
     let mut mem: [u8; 100] = [0; 100];
     let mut ip: usize = 0;
     let mut i: usize = 0;
     let mut open_brackets: u8;
-    while i < bf_code.len() {
-        match &bf_code[i..i + 1] {
-            "+" => mem[ip] += 1,
-            "-" => {
+    while i < instructions.len() {
+        match instructions[i] {
+            '+' => mem[ip] += 1,
+            '-' => {
                 if mem[ip] > 0 {
                     mem[ip] -= 1
                 }
             }
-            ">" => {
+            '>' => {
                 if ip != 99 {
                     ip += 1;
                 }
             }
-            "<" => {
+            '<' => {
                 if ip != 0 {
                     ip -= 1;
                 }
             }
-            "." => {
+            '.' => {
                 println!("{}", mem[ip] as char);
             }
-            "," => {
+            ',' => {
                 let mut input = String::new();
                 std::io::stdin()
                     .read_line(&mut input)
-                    .ok()
                     .expect("Failed to read line");
-                mem[ip] = input.bytes().nth(0).expect("no byte read");
+                mem[ip] = *input.as_bytes().first().expect("no byte read");
             }
-            "[" => {
+            '[' => {
                 if mem[ip] == 0 {
-                    while &bf_code[i..i + 1] != "]" {
+                    while instructions[i] != ']' {
                         i += 1;
                     }
                 }
             }
-            "]" => {
+            ']' => {
                 if mem[ip] != 0 {
                     open_brackets = 1;
                     while open_brackets > 0 {
-                        if &bf_code[i..i + 1] == "[" {
+                        if instructions[i] == '[' {
                             open_brackets -= 1;
                         }
                         i -= 1;
-                        if &bf_code[i..i + 1] == "]" {
+                        if instructions[i] == ']' {
                             open_brackets += 1;
                         }
                     }
